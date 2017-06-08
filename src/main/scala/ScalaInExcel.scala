b24b4e5eef587b8913451870f4f078dc8744565c
cpluspluscrowe <ccrowe@unomaha.edu>
2017-06-08 08:58:23 -0500

    Added method: File control level for workbooks.  This is a new way of opening/closing workbooks

--- src/main/scala/ScalaInExcel.scala
+++ src/main/scala/ScalaInExcel.scala
@@ -1,19 +1,27 @@
 /**
   * Created by CCROWE on 6/8/2017.
   */
-import java.io.FileOutputStream
+import java.io.{File, FileInputStream, FileOutputStream}
 import java.util.{Calendar, Date}
 
 import com.norbitltd.spoiwo.model.enums.CellFill
 import com.norbitltd.spoiwo.model._
 import com.norbitltd.spoiwo.natures.xlsx.Model2XlsxConversions._
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.openxml4j.opc.OPCPackage
+import org.apache.poi.poifs.filesystem.NPOIFSFileSystem
+import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.apache.poi.ss.util.WorkbookUtil
 import org.apache.poi.xssf.usermodel.{XSSFCell, XSSFRow, XSSFWorkbook}
 import org.joda.time.LocalDate
 
 object ScalaInExcel {
   def main(args: Array[String]) {
+    var wb = WorkbookFactory.create(new File("workbook.xlsx"));
+    wb = WorkbookFactory.create(new FileInputStream("workbook.xlsx"));
+    var pkg = OPCPackage.open(new File("workbook.xlsx"))
+    wb = new XSSFWorkbook(pkg);
+    pkg.close();
 
   }
 }
--- src/main/scala/Workbook.scala
+++ src/main/scala/Workbook.scala
@@ -1,5 +1,7 @@
-import java.io.FileOutputStream
+import java.io.{File, FileInputStream, FileOutputStream}
 
+import org.apache.poi.openxml4j.opc.OPCPackage
+import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.apache.poi.ss.util.WorkbookUtil
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
@@ -7,5 +9,11 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook
   * Created by CCROWE on 6/8/2017.
   */
 object Workbook {
-
+  def FileWorkbook(): Unit ={ //full control over workbook lifecycle
+    var wb = WorkbookFactory.create(new File("workbook.xlsx"));
+    wb = WorkbookFactory.create(new FileInputStream("workbook.xlsx"));
+    var pkg = OPCPackage.open(new File("workbook.xlsx"))
+    wb = new XSSFWorkbook(pkg);
+    pkg.close();
+  }
 }
