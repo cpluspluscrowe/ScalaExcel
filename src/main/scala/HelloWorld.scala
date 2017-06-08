366fedde13a17a974e35bbf287208ee6ba2cb724
cpluspluscrowe <ccrowe@unomaha.edu>
2017-06-08 08:28:58 -0500

    Added method: Manipulates sheet cells

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -16,6 +16,22 @@ object HelloWorld {
   def main(args: Array[String]) {
 
   }
+  def SetCellValues(): Unit ={
+    var wb = new XSSFWorkbook();
+    var createHelper = wb.getCreationHelper();
+    var sheet = wb.createSheet("new sheet");
+    var row = sheet.createRow(0);
+    var cell = row.createCell(0);
+    cell.setCellValue(1);
+    row.createCell(1).setCellValue(1.2);
+    row.createCell(2).setCellValue(
+      createHelper.createRichTextString("This is a string")
+    );
+    row.createCell(3).setCellValue(true);
+    var fileOut = new FileOutputStream("workbook.xlsx");
+    wb.write(fileOut);
+    fileOut.close();
+  }
   def CreateSheets(): Unit ={
     var wb = new XSSFWorkbook();
     var sheet1 = wb.createSheet("new sheet");
